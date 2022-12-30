b924c5f3f45af0c5e3692e3f2906ffa49d2c1449
Tej Agarwal <rocketphoenix44@gmail.com>
2022-12-31 04:01:53 +0530

    logging added

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+import logging
+
+logging.basicConfig(filename='client_data.log', level=logging.DEBUG)
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    logging.info("Requester ip: " + request.remote_addr)
     return "<p>Hello, World!</p>"
